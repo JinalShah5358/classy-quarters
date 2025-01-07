9f0a09912fa4aa435efcb4960c13e769744036a6
171201 <jinalshah5358@gmail.com>
2025-01-07 22:33:17 +0530

    add toast and delete cabin logic

--- src/services/apiCabins.js
+++ src/services/apiCabins.js
@@ -9,3 +9,12 @@ export async function getCabins() {
 
   return data;
 }
+
+export async function deleteCabin(id) {
+  let { data, error } = await supabase.from("cabins").delete().eq("id", id);
+  if (error) {
+    console.log(error);
+    throw new Error("Cabin can't be deleted");
+  }
+  return data;
+}
